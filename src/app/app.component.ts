2106be0fecaf0e134c8d6e55314a60b55fdde998
Alex <alex.varghese77@gmail.com>
2018-10-08 10:01:44 -0800

    news app almost completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { NewsApiService } from './news-api.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  mArticles: Array<any>;
+  mSources: Array<any>;
+
+  constructor(private newsapi: NewsApiService) { }
+
+  ngOnInit() {
+    // Load articles
+    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
+    //Load sourse
+    this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
+  }
+
+  searchArticles(source) {
+    this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
+  }
 }
+
